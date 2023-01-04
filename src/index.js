d0c76d20bf51c9c2b992e432c499f879f7baa67e
Ash <ash@midiidev.xyz>
2023-01-04 07:45:47 -0500

    https

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
 const app = express()
 
 // load env vars
@@ -8,6 +11,24 @@ require('dotenv').config()
 require('./routes')(app)
 
 // start the app
-app.listen(process.env.PORT ?? 3000, () => {
-    console.log(`App started on port ${process.env.PORT ?? 3000}`)
-})
+if ((process.env.SERVER_TYPE == 'https')) {
+    https
+        .createServer(
+            {
+                key: fs.readFileSync(
+                    path.join(__dirname, '/../server/server.key')
+                ),
+                cert: fs.readFileSync(
+                    path.join(__dirname, '/../server/server.cert')
+                )
+            },
+            app
+        )
+        .listen(process.env.PORT ?? 3000, () => {
+            console.log(`App started on port ${process.env.PORT ?? 3000} (https)`)
+        })
+} else {
+    app.listen(process.env.PORT ?? 3000, () => {
+        console.log(`App started on port ${process.env.PORT ?? 3000}`)
+    })
+}
